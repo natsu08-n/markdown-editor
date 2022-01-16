531c9223fba48306e830835603de9254726cbcea
natsu08-n <merrysheep18@gmail.com>
2022-01-16 16:35:36 +0900

    react-router を導入して複数ページの遷移可能に
    - editorページとhistoryページの２ページを遷移できるように変更

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
-import * as React from "react";
-import { render } from "react-dom";
-import styled from 'styled-components';
+import * as React from "react"
+import { render } from "react-dom"
+import styled from 'styled-components'
 import { createGlobalStyle } from 'styled-components' //ページ全体に適用できるスタイルを定義
+import {
+	HashRouter as Router,
+	Switch,
+	Route,
+	Redirect,
+}	from 'react-router-dom'
 import { Editor } from './pages/editor'
+import { History } from "./pages/history"
+
 
 const GlobalStyle = createGlobalStyle`
 body * {
@@ -12,7 +20,17 @@ body * {
 const Main = (
 	<>
 	<GlobalStyle/>
-	<Editor/>
+	<Router>
+	{/*exact 属性は、パスの判定方法を切り替えるパラメーター、exact={true} の場合は完全一致、exact={false} は部分一致*/}
+		<Route exact path="/editor">
+			<Editor/>
+		</Route>
+		<Route exact path="/history">
+			<History />
+		</Route>
+		{/*定義されていないパスの場合は /editor にリダイレクト*/}
+		<Redirect to="/editor" path="*"/>
+	</Router>
 	</>
 )
 

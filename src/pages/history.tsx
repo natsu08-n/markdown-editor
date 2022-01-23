4051a0cb9f37944e7dfc8e0b24d11e614192a76f
natsu08-n <merrysheep18@gmail.com>
2022-01-23 16:13:20 +0900

    テキスト履歴を表示

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -28,10 +28,31 @@ const Wrapper= styled.div`
 	padding: 0 1rem;
 `
 
+const Memo = styled.button`
+	display: block;
+	background-color: white;
+	border: 1px solid gray;
+	width: 100%;
+	padding: 1rem;
+	margin: 1rem 0;
+	text-align: left;
+`
+
+const MemoTitle = styled.div`
+	font-size: 1rem;
+	margin-bottom: 0.5rem;
+`
+// overflow: hidden;でオーバーした要素を非表示にする、text-overflow: ellipsis;でオーバーしたテキストを３点リーダーにする、white-space: nowrap;で改行を半角スペースに変換することで、1行にする
+const MemoText = styled.div`
+	font-size: 0.85rem;
+	overflow: hidden;
+	text-overflow: ellipsis;
+	white-space: nowrap;
+`
+
 export const History: React.FC = () => {
 
 	const [memos, setMemos] = useState<MemoRecord[]>([])
-	console.log(memos);
 
 	//useEffectは副作用フック、第一引数に実行したい関数(つまりgetMemos)を渡してその非同期処理が終わったら取得したテキスト履歴をsetMemosに渡して更新している
 	useEffect(() => {
@@ -48,7 +69,12 @@ export const History: React.FC = () => {
 			</Header>
 		</HeaderArea>
 		<Wrapper>
-			TODO: 履歴表示
+		{memos.map(memo => (
+			<Memo key={memo.datetime}>
+				<MemoTitle>{memo.title}</MemoTitle>
+				<MemoText>{memo.text}</MemoText>
+			</Memo>
+		))}
 		</Wrapper>
 		</>
 	)

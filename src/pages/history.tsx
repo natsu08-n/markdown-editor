9e5338f3ede926008eea143a0505501a641251d8
natsu08-n <merrysheep18@gmail.com>
2022-02-03 23:34:41 +0900

    ページング操作

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -6,6 +6,7 @@ import {
 import styled from 'styled-components'
 import { Header } from '../components/header'
 import {
+	getMemoPageCount,
 	getMemos,
 	MemoRecord,
 } from '../indexeddb/memos'
@@ -19,13 +20,14 @@ const HeaderArea = styled.div`
 	left: 0;
 `
 
-const Wrapper= styled.div`
-	bottom: 0;
+const Wrapper = styled.div`
+	bottom: 3rem;
 	left: 0;
 	position: fixed;
 	right: 0;
 	top: 3rem;
 	padding: 0 1rem;
+	overflow-y: scroll;
 `
 
 const Memo = styled.button`
@@ -36,6 +38,28 @@ const Memo = styled.button`
 	padding: 1rem;
 	margin: 1rem 0;
 	text-align: left;
+`
+
+const Paging = styled.div`
+	bottom: 0;
+	height: 3rem;
+	left: 0;
+	line-height: 2rem;
+	padding: 0.5rem;
+	position: fixed;
+	right: 0;
+	text-align: center;
+`
+
+const PagingButton = styled.button`
+	background: none;
+	border: none;
+	display: inline-block;
+	height: 2rem;
+	padding: 0.5rem 1rem;
+	&:disabled {
+		color: silver;
+	}
 `
 
 const MemoTitle = styled.div`
@@ -57,36 +81,64 @@ interface Props {
 export const History: React.FC<Props> = (props) => {
 	const { setText } = props
 	const [memos, setMemos] = useState<MemoRecord[]>([])
+	const [page, setPage] = useState(1)
+	const [maxPage, setMaxPage] = useState(1)
 	const history = useHistory()
 
 	//useEffectは副作用フック、第一引数に実行したい関数(つまりgetMemos)を渡してその非同期処理が終わったら取得したテキスト履歴をsetMemosに渡して更新している
 	useEffect(() => {
-		getMemos().then(setMemos)
+		getMemos(1).then(setMemos)
+		getMemoPageCount().then(setMaxPage)
 	}, [])
+
+	const canNextPage: boolean = page < maxPage
+	const canPrevPage: boolean = page > 1
+	const movePage = (targetPage: number) => {
+		if (targetPage < 1 || maxPage < targetPage) {
+			return
+		}
+		setPage(targetPage)
+		getMemos(targetPage).then(setMemos)
+	}
 
 	return (
 		<>
-		<HeaderArea>
-			<Header title="履歴">
-				<Link to="/editor">
-					エディタに戻る
-				</Link>
-			</Header>
-		</HeaderArea>
-		<Wrapper>
-		{memos.map(memo => (
-			<Memo 
-				key={memo.datetime}
-				onClick={() => {
-					setText(memo.text)
-					history.push('/editor')
-				}}
+			<HeaderArea>
+				<Header title="履歴">
+					<Link to="/editor">
+						エディタに戻る
+					</Link>
+				</Header>
+			</HeaderArea>
+			<Wrapper>
+				{memos.map(memo => (
+					<Memo
+						key={memo.datetime}
+						onClick={() => {
+							setText(memo.text)
+							history.push('/editor')
+						}}
+					>
+						<MemoTitle>{memo.title}</MemoTitle>
+						<MemoText>{memo.text}</MemoText>
+					</Memo>
+				))}
+			</Wrapper>
+			<Paging>
+				<PagingButton
+					onClick={() => movePage(page - 1)}
+					disabled={!canPrevPage}
 				>
-				<MemoTitle>{memo.title}</MemoTitle>
-				<MemoText>{memo.text}</MemoText>
-			</Memo>
-		))}
-		</Wrapper>
+					＜＜
+				</PagingButton>
+				{page} / {maxPage}
+				<PagingButton
+					onClick={() => movePage(page + 1)}
+					disabled={!canNextPage}
+				>
+					＞＞
+				</PagingButton>
+			</Paging>
 		</>
 	)
 }
